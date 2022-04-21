e19aff1ba3a266fa8033f72439f03c86a4cc7680
『 Matteo Santoro 』 <65976958+mega2799@users.noreply.github.com>
2022-04-21 20:18:31 +0200

    added Java project

---
+++ Es2/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.es2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.es2 to javafx.fxml;
+    exports com.example.es2;
+}
